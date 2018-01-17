c9f7baed859344e4e95eea98c017df7072bfee7d
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-17 10:58:04 -0500

    Updated from Brython Server: 1/17/2018 10:58:04 AM

--- lineequation.py
+++ lineequation.py
@@ -1,1 +1,13 @@
+#Sam Krimmel
+#1/17/18
+#lineequation - finds the equation of a line from two given points
+
+x1 = int(input('x1 = '))
+y1 = int(input('y1 = '))
+x2 = int(input('x2 = '))
+y2 = int(input('y2 = '))
+
+print('y=mx+b')
+
+m = (y2-y1)/(x2-x1)
 
