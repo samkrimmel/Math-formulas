ace07c6ecebac98229582e3877bb3dfd69dea7d7
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-17 19:08:21 -0500

    Updated from Brython Server: 1/17/2018 7:08:20 PM

--- lineequation.py
+++ lineequation.py
@@ -7,7 +7,13 @@ y1 = int(input('y1 = '))
 x2 = int(input('x2 = '))
 y2 = int(input('y2 = '))
 
-print('y=mx+b')
 
-m = (y2-y1)/(x2-x1)
+print('y=mx+b')
+if (x2-x1) == 0:
+    print('Vertical line, not a function')
+elif (y2-y1) == 0:
+    print('Horizontal line at y = ',y1)
+else :
+    m = ((y2-y1)/(x2-x1))
+    print('m = ',m,'and b = ',(y1-(x1*m)))
 
