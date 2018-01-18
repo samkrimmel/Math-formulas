f7a8587380b0097bb5d56fa25c4e0dc7b5e74de0
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-18 13:57:13 -0500

    Updated from Brython Server: 1/18/2018 1:57:11 PM

--- loopexample.py
+++ loopexample.py
@@ -1,1 +1,17 @@
+#Sam Krimmel
+#1/18/18
+#how to use a loop
 
+"""for i in range(1,101):
+    if i%7 == 0 or '7' in str(i):
+        print('Buzz')
+    else:
+        print(i)"""
+
+i = 1
+while i <= 100:
+    if i%7 == 0 or '7' in str(i):
+        print('Buzz')
+    else:
+        print(i)
+    i = i+1
