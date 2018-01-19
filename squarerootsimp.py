e22c0fa0370dc1420e551b08d105b3f8072a8d03
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-19 13:15:14 -0500

    Update squarerootsimp.py

--- squarerootsimp.py
+++ squarerootsimp.py
@@ -1,1 +1,14 @@
+#Sam Krimmel
+#1/19/18
+#squarerootsimp.py - simplifies the square root of a number
+
+from math import sqrt
+
+a = int(input('Simplify the square root of: '))
+s = sqrt(1)
+for x in range(a,1,-1):
+    if a%x == 0 and sqrt(x).is_integer():
+        s = x
+print(sqrt(s),'square root',a/s)
+
 
