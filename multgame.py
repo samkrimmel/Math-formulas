d137a7dfced90f7e2888418664601890b2daf902
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-24 10:57:41 -0500

    Updated from Brython Server: 1/24/2018 10:57:37 AM

--- multgame.py
+++ multgame.py
@@ -4,22 +4,16 @@
 
 from random import randint
 
-num1 = randint(1,10)
-num2 = randint(1,10)
 ques = 0
 
-print('What is:', str(num1),'x', str(num2))
 
-ans = int(input('= '))
-
-
-if ans == (num1*num2):
-    print('Correct!')
-    ques + 1
-else:
-    print('Nope')
-    
-for ques in range(1,6):
+while ques < 6:
+    num1 = randint(1,10)
+    num2 = randint(1,10)
     print('What is:', str(num1),'x', str(num2))
-    
-
+    ans = int(input('= '))
+    if ans == (num1*num2):
+        print('Correct!')
+        ques = ques+1
+    else:
+        print('Nope')
